2c1de06eaa5b7227a0bd6f9cd913e338d1dcde24
reducedcarpet <michaelcadenpike@gmail.com>
2024-04-19 16:42:09 +0100

    refactor: Moving strings out to xml file

--- app/src/main/java/net/firestaff/mcp/gymsharkhomework/ui/lists/ProductList.kt
+++ app/src/main/java/net/firestaff/mcp/gymsharkhomework/ui/lists/ProductList.kt
@@ -6,8 +6,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.google.gson.Gson
+import net.firestaff.mcp.gymsharkhomework.R
 import net.firestaff.mcp.gymsharkhomework.models.Product
 import net.firestaff.mcp.gymsharkhomework.ui.ProductListTile
 import java.net.URLEncoder
@@ -17,18 +19,16 @@ fun ProductList(products: List<Product>, navController: NavController) {
 
     Column(modifier = Modifier.padding(16.dp)) {
         Text(
-            text = "Products: " ,
-            modifier = Modifier.padding(vertical = 16.dp)
+            text = stringResource(id = R.string.products),
+            modifier = Modifier.padding(vertical = 16.dp),
         )
         products.forEach { product ->
             ProductListTile(
                 title = product.title,
                 trailing = { Text(product.price.toString()) },
                 onClick = {
-                    val gson = Gson()
-                    val productJson = gson.toJson(product)
                     navController.navigate(
-                        "productDetail/${URLEncoder.encode(productJson, "UTF-8")}"
+                        "productDetail/${product.id}"
                     )
                 },
                 //leading = { ProductImage(url = product.url, productName = product.title) }
